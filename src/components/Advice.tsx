44c3224c35678292023f780bab4140717c6f6d45
Nnadi Victory <chikamagozi25@gmail.com>
2023-03-22 01:39:54 +0100

    bug fix

--- src/components/Advice.tsx
+++ src/components/Advice.tsx
@@ -1,17 +1,13 @@
-import line from '../assets/pattern-divider-desktop.svg'
+import line from '../assets/pattern-divider-desktop.svg';
 import { AdviceQuery } from '../App';
 
-
-
-
-const Advice = ({id, advice}: AdviceQuery) => {
-
+const Advice = ({ id, advice }: AdviceQuery) => {
 	return (
 		<>
 			<p className='text-neonGreen text-xs font-extrabold'>ADVICE #{id}</p>
-            <p className='text-white text-xl'>"{advice}"</p>
-            <div id="divider">
-            <img src={line} />
+			<p className='text-white text-xl'>"{advice}"</p>
+			<div id='divider'>
+				<img src={line} />
 			</div>
 		</>
 	);
