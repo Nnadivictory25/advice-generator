b0676ce7f2a3a2db90299743f4359b388e81f0f2
Nnadi Victory <chikamagozi25@gmail.com>
2023-03-22 01:45:33 +0100

    bug fix

--- src/components/GeneratorBtn.tsx
+++ src/components/GeneratorBtn.tsx
@@ -1,12 +1,15 @@
 import dice from '../assets/icon-dice.svg';
 
 interface Props {
-    onClick: () => void;
+	onClick: () => void;
 }
 
-const GeneratorBtn = ({onClick}: Props) => {
+const GeneratorBtn = ({ onClick }: Props) => {
 	return (
-		<div onClick={onClick} id='genBtn' className='rounded-full bg-neonGreen h-[50px] w-[50px] items-center absolute cursor-pointer flex justify-center'>
+		<div
+			onClick={onClick}
+			id='genBtn'
+			className='rounded-full bg-neonGreen h-[50px] w-[50px] items-center absolute cursor-pointer flex justify-center'>
 			<button>
 				<img src={dice} />
 			</button>
